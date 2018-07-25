839dc1d9aa39e66839fa6517bfb6bd9e9644cb13
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-25 22:55:28 +1000

    css experimentation

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react"
 import "./App.css"
+import logo from "./logo.svg"
 
 class App extends Component {
   render() {
     return (
       <div id="container">
-        <aside id="sidebar">Users</aside>
+        <aside id="sidebar">
+          Users
+          <div id="logo-box">
+            <img src={logo} id="App-logo" alt="logo" />
+          </div>
+        </aside>
         <section id="main">
           <section id="messages-list">Messages list</section>
           <section id="new-message">New message</section>
